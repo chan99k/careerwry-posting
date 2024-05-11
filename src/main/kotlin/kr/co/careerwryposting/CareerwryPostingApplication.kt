373d72fb6b79f0ad9961cab1257d9c26dde0264e
cgk95 <kjkj5868@gmail.com>
2024-05-11 18:58:53 +0900

    init :: 프로젝트 생성

---
+++ src/main/kotlin/kr/co/careerwryposting/CareerwryPostingApplication.kt
@@ -0,0 +1,11 @@
+package kr.co.careerwryposting
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CareerwryPostingApplication
+
+fun main(args: Array<String>) {
+    runApplication<CareerwryPostingApplication>(*args)
+}
---
+++ src/test/kotlin/kr/co/careerwryposting/CareerwryPostingApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.co.careerwryposting
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CareerwryPostingApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
