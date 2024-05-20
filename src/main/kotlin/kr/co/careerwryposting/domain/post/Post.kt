8cb7f5693ecb5295026fa3d98237b001948be0fe
cgk95 <kjkj5868@gmail.com>
2024-05-20 16:03:23 +0900

    chore :: 유저 프로필 관련 정보를 값 객체로 추출

--- src/main/kotlin/kr/co/careerwryposting/domain/post/Post.kt
+++ src/main/kotlin/kr/co/careerwryposting/domain/post/Post.kt
@@ -5,6 +5,13 @@ import kr.co.careerwryposting.common.util.TokenGenerator
 import kr.co.careerwryposting.domain.AbstractEntity
 import kr.co.careerwryposting.interfaces.post.PostDto
 
+@Embeddable
+data class UserProfile(
+    var nickName: String,
+    var positionJob: String,
+    var profileImage: String
+)
+
 @Entity
 class Post(
     @Column(nullable = false)
@@ -12,6 +19,9 @@ class Post(
 
     @Column(nullable = false)
     var content: String,
+
+    @Embedded
+    var profile: UserProfile,
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -42,10 +52,18 @@ class Post(
         fun fixture(
             title: String = "제목",
             content: String = "본문",
-        ): Post {
+            nickName: String = "사용자 닉네임",
+            positionJob: String = "프론트 엔드",
+            profileImage: String = "https://e7.pngegg.com/pngimages/1000/665/png-clipart-computer-icons-profile-s-free-angle-sphere.png",
+            ): Post {
             return Post(
                 title = title,
                 content = content,
+                UserProfile(
+                    nickName = nickName,
+                    positionJob = positionJob,
+                    profileImage = profileImage,
+                )
             )
         }
     }
@@ -53,5 +71,13 @@ class Post(
 
 /**
  * Note
- * 제목, 본문, 첨부 이미지(5개까지), 토큰(식별자)
+ *     {
+ *       nickName: '개개발발자자',
+ *       positionJob: '어딘가의 프론트엔드 개발자', -> 이거는 멤버 서비스에 api 요청해서 받아와야 함 : 리액티브
+ *       profileImg: 'https://e7.pngegg.com/pngimages/1000/665/png-clipart-computer-icons-profile-s-free-angle-sphere.png',
+ *       -> 사용자 정보 -> fixture 를 만들 때 security context 에서 token 정보를 추출하여 만들기
+ *       date: '5월 3일', // created or modified date (형태는 YYYY.MM.DD가 제일 나을듯?)
+ *       title: '우와아앙',
+ *       contents: '이것이 첫글이지롱',
+ *     },
  */
