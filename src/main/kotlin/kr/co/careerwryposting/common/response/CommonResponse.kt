21c13132958ed3ca10653a15c6462f1e8e7dbc3f
cgk95 <kjkj5868@gmail.com>
2024-05-20 22:15:13 +0900

    feat :: 커스텀 http 응답 객체 생성

---
+++ src/main/kotlin/kr/co/careerwryposting/common/response/CommonResponse.kt
@@ -0,0 +1,48 @@
+package kr.co.careerwryposting.common.response
+
+import org.springframework.http.HttpStatus
+
+data class CommonResponse<T>(
+    val result: Result,
+    val data: T? = null,
+    val message: String? = null,
+    val status: HttpStatus = HttpStatus.OK,
+) {
+
+    companion object {
+        fun <T> success(data: T, message: String? = null): CommonResponse<T> {
+            return CommonResponse(
+                result = Result.SUCCESS,
+                data = data,
+                message = message
+            )
+        }
+
+        fun <T> success(data: T, status: HttpStatus): CommonResponse<T> {
+            return CommonResponse(
+                result = Result.SUCCESS,
+                data = data,
+                status = status,
+            )
+        }
+
+        fun fail(message: String, status: HttpStatus): CommonResponse<Nothing> {
+            return CommonResponse(
+                result = Result.FAIL,
+                message = message,
+                status = status
+            )
+        }
+
+        fun fail(errorCode: HttpStatus): CommonResponse<Nothing> {
+            return CommonResponse(
+                result = Result.FAIL,
+                status = errorCode,
+            )
+        }
+    }
+
+    enum class Result {
+        SUCCESS, FAIL
+    }
+}
