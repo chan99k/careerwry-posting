e76330c71fe32c59a9f4bd44feec16c195fcfe5f
cgk95 <kjkj5868@gmail.com>
2024-05-20 22:15:33 +0900

    feat :: 커스터 예외관리 구현

---
+++ src/main/kotlin/kr/co/careerwryposting/common/response/ErrorCode.kt
@@ -0,0 +1,13 @@
+package kr.co.careerwryposting.common.response
+
+import org.springframework.http.HttpStatus
+
+enum class ErrorCode(
+    val status: HttpStatus,
+    val message: String,
+) {
+    COMMON_SYSTEM_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "일시적인 오류가 발생했습니다. 잠시 후 다시 시도해주세요"), // 장애 상황
+    COMMON_INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "요청한 값이 올바르지 않습니다"),
+    POST_INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "필수적인 JSON Property를 찾을 수 없습니다"),
+    COMMON_ENTITY_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 엔티티입니다"), ;
+}
